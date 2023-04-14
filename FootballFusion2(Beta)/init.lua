4ea98c91e3decac260a92ff2dc9de291a6153fc1
tyler <63240641+itstylerrr@users.noreply.github.com>
2023-04-13 21:08:16 -0500

    getting buttons to work

--- FootballFusion2(Beta)/init.lua
+++ FootballFusion2(Beta)/init.lua
@@ -15,7 +15,7 @@ Variables.Library = loadstring(game:HttpGet"https://raw.githubusercontent.com/da
 
 -- Build main UI
 
-Variables.Window = Variables.Library:Window("Greezy Hub", "v0.5.1", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+Variables.Window = Variables.Library:Window("Greezy Hub", "v0.5.3", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
 
 Variables.CatchingTab = Variables.Window:Tab("Catching", "http://www.roblox.com/asset/?id=6023426915")
 Variables.SwattingTab = Variables.Window:Tab("Swatting", "http://www.roblox.com/asset/?id=6023426915")
@@ -23,23 +23,22 @@ Variables.Physics = Variables.Window:Tab("Physics", "http://www.roblox.com/asset
 Variables.Kicking = Variables.Window:Tab("Kicking", "http://www.roblox.com/asset/?id=6023426915")
 
 Variables.CatchingLabel = Variables.CatchingTab:Label("Magging")
-Variables.CatchingTab:Line()
 Variables.MagToggle = Variables.CatchingTab:Toggle("Toggle Mags", "Toggle mags on and off.", function(t)
-    print("Mag Toggle: " .. toString(t))
+    print("Mag Toggled")
 end
 )
 Variables.MagSlider = Variables.CatchingTab:Slider("Mag Distance", "The distace for mags.", 1, 40, 7, function(t)
-    print("Slider Value: " .. toString(t))
+    print("Slider Value: ")
+    print(t)
 end
 )
 Variables.CatchingTab:Line()
 
 Variables.GraphingLabel = Variables.CatchingTab:Label("Graphing")
 Variables.GraphingToggle = Variables.CatchingTab:Toggle("Toggle Graph", "Toggle the graph on and off.", function(t)
-    print("Graph Toggle: " .. toString(t))
+    print("Graph Toggled")
 end
 )
-Variables.CatchingTab:Line()
 
 
 -- Bootup
